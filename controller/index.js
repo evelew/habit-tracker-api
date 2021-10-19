cc92076b971650c4599d5ab78303ca00d83b60b9
Evellyn Lima <evellynlrl@hotmail.com>
2021-10-19 20:26:57 -0300

    First commit

---
+++ controller/index.js
@@ -0,0 +1,12 @@
+// const getAll = require('./../services/get-all')
+
+module.exports = (express) => {
+  let api = express.Router()
+
+  // api.get('/income/get-all', async function (req, res) {
+  //   const incomes = await getAll()
+  //   res.send(incomes)
+  // })
+
+  return api
+}
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3001
+const controller = require('./controller')(express)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cors())
+
+app.use('/', controller)
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}!`)
+})
