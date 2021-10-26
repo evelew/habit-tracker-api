f526b2a0f67f087c4b738a276bfa922eea12a609
Evellyn Lima <evellynlrl@hotmail.com>
2021-10-26 09:34:14 -0300

    Add Joi to set a schema to the inputs. Add package.json file.

--- services/add-new.js
+++ services/add-new.js
@@ -1,6 +1,26 @@
+const Joi = require('joi')
+
 const addNew = require('./../repository/add-new')
 
 module.exports = async (body) => {
+  const schema = Joi.object({
+    name: Joi.string().alphanum().max(30).required(),
+    description: Joi.string().alphanum().max(80),
+    required_days: Joi.object({
+      sunday: Joi.boolean(),
+      monday: Joi.boolean(),
+      tuesday: Joi.boolean(),
+      wednesday: Joi.boolean(),
+      thursday: Joi.boolean(),
+      friday: Joi.boolean(),
+      saturday: Joi.boolean(),
+    }).required(),
+    has_reminder: Joi.boolean().required(),
+    reminder_hour: Joi.date(), // TODO: usar essa chave pra definir horário do lembrete
+    reminder_note: Joi.string().max(80),
+    status: Joi.string().valid('working', 'done', 'waiting').required(),
+  })
+
   const data = {
     name: body.name,
     description: body.description,
@@ -12,5 +32,6 @@ module.exports = async (body) => {
   }
 
   const message = await addNew(data)
+
   return message
 }
