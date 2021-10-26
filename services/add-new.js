647248e0e586f2b857c92ec30a5dbaf971be01d2
Evellyn Lima <evellynlrl@hotmail.com>
2021-10-26 20:03:10 -0300

    Add folder: schemas

--- services/add-new.js
+++ services/add-new.js
@@ -1,26 +1,7 @@
-const Joi = require('joi')
-
 const addNew = require('./../repository/add-new')
+const schema = require('./../schemas/habit')
 
 module.exports = async (body) => {
-  const schema = Joi.object({
-    name: Joi.string().max(30).required(),
-    description: Joi.string().max(80),
-    required_days: Joi.object({
-      sunday: Joi.boolean(),
-      monday: Joi.boolean(),
-      tuesday: Joi.boolean(),
-      wednesday: Joi.boolean(),
-      thursday: Joi.boolean(),
-      friday: Joi.boolean(),
-      saturday: Joi.boolean(),
-    }).required(),
-    has_reminder: Joi.boolean().required(),
-    reminder_hour: Joi.date(), // TODO: usar essa chave pra definir horário do lembrete
-    reminder_note: Joi.string().max(80),
-    status: Joi.string().valid('working', 'done', 'waiting').required(),
-  })
-
   const { error, value } = schema.validate(body, {
     abortEarly: false,
   })
