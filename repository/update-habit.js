532d87a98ca0b3a8cb5ddfef4372b6cf4ce14052
Evellyn Lima <evellynlrl@hotmail.com>
2021-10-30 15:49:13 -0300

    Add error message to the update endpoint

--- repository/update-habit.js
+++ repository/update-habit.js
@@ -3,9 +3,14 @@ const db = require('./../_db')
 const updateHabit = async ({ _id, body }) => {
   const Habit = db.Mongoose.model('habits', db.HabitSchema)
 
-  await Habit.updateOne({ _id }, body)
-
-  return `Hábito atualizado com sucesso!`
+  return Habit.updateOne({ _id }, body)
+    .then(() => {
+      return 'Hábito atualizado com sucesso!'
+    })
+    .catch((err) => {
+      console.error('ERR:', err)
+      return 'ERRO: Alguma coisa deu errado!'
+    })
 }
 
 module.exports = updateHabit
